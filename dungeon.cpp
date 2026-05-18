2b6bf41588ee207fc0e9f87a8b8713ce4bae5a96
Melissa Marcaida <melmarcaida1@gmail.com>
2026-05-18 16:03:24 +0800

    add player movement logic & boundary collision

--- dungeon.cpp
+++ dungeon.cpp
@@ -14,14 +14,78 @@ struct Player {
   int health;
 };
 
+void initializeMap(char map[][COLS]);
+void displayMap(char map[][COLS], struct Player p);
 
 int main() {
     
     Player p = {1, 1, 100};
-
     char map[ROWS][COLS];
+    bool isRunning = true;
+    char keyInput;
 
-    cout << "Dungeon skeleton compiled successfully." << endl;
+    initializeMap(map);
+
+    while (isRunning) {
+      displayMap(map, p);
+      cout << "Enter a move (W/A/S/D): ";
+      cin >> keyInput;
+
+      // positioning of P
+      switch (keyInput) {
+      case 'W': case 'w':
+        if (p.y > 0) {
+          p.y--;
+        }
+        break;
+      case 'S': case 's':
+        if (p.y < ROWS - 1) {
+          p.y++;
+        }
+        break;
+      case 'A': case 'a':
+        if (p.x > 0) {
+          p.x--;
+        }
+        break;
+      case 'D': case 'd':
+        if (p.x < COLS - 1) {
+          p.x++;
+        }
+        break;
+      case 'Q': case 'q':
+        cout << "You have exited the game.\n";
+        isRunning = false;
+        break;
+      default:
+        cout << "Notice: Press valid key (W/A/S/D/Q) | Q = Exit\n";
+        break;
+      }
+    }
+    
 
     return 0;
+}
+
+void initializeMap(char map[][COLS]) {
+  for (int i = 0; i < ROWS; i++) { // !rows = Y axis
+    for (int j = 0; j < COLS; j++) { // !columns = X axis 
+      map[i][j] = '.';
+    }
+  }
+}
+
+void displayMap(char map[][COLS], struct Player p) {
+  for (int i = 0; i < ROWS; i++) { // !rows = Y axis
+    for (int j = 0; j < COLS; j++) { // !columns = X axis 
+      if (i == p.y && j == p.x) {
+        cout << "P";
+      }
+      else {
+        cout << map[i][j];
+      }
+      
+    }
+    cout << "\n";
+  }
 }
